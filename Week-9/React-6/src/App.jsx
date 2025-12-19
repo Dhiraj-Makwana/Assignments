f066062f3d94a39278b45ea20788131a981440cf
Dhiraj-Makwana <dhirajmakwana33912@gmail.com>
2025-12-19 18:36:51 +0530

    Add MyComponent function to learn inline Styling

--- Week-9/React-6/src/App.jsx
+++ Week-9/React-6/src/App.jsx
@@ -6,6 +6,8 @@ function App() {
         <Todo key={1} todo={"Go to gym"} done={false} />,
         <Todo key={2} todo={"Have a breakfast"} done={true}/>
       ]}
+
+      <MyComponent/>
     </div>
   )
 }
@@ -13,6 +15,12 @@ function App() {
 function Todo({ todo, done }) {
   return <div>
     {todo} - {done ? "Done" : "Not Done"}
+  </div>
+}
+
+function MyComponent() {
+  return <div style={{background: "navy", color: "white", padding: 10, borderRadius: 13}}>
+    Hello World
   </div>
 }
 
