16b16a028a096e055363a8fda5478bced92a049e
Dhiraj-Makwana <dhirajmakwana33912@gmail.com>
2025-12-19 18:34:30 +0530

    Add Todo function

--- Week-9/React-6/src/App.jsx
+++ Week-9/React-6/src/App.jsx
@@ -2,9 +2,18 @@ function App() {
 
   return (
     <div>
-      Hello world
+      {[
+        <Todo key={1} todo={"Go to gym"} done={false} />,
+        <Todo key={2} todo={"Have a breakfast"} done={true}/>
+      ]}
     </div>
   )
+}
+
+function Todo({ todo, done }) {
+  return <div>
+    {todo} - {done ? "Done" : "Not Done"}
+  </div>
 }
 
 export default App
