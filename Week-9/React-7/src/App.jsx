41698632665de697aab76f24d94cee8ac703ad8c
Dhiraj-Makwana <dhirajmakwana33912@gmail.com>
2025-12-21 16:23:11 +0530

    Routing step-1: Render specific content to the specific Route is done

--- Week-9/React-7/src/App.jsx
+++ Week-9/React-7/src/App.jsx
@@ -1,10 +1,36 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 function App() {
 
   return (
     <div>
-      Hello, I am starting to learn Routing in react
+      <BrowserRouter>
+        <Routes>
+          <Route path="/neet/online-coaching-class-11" element={<Class11Program />} />
+          <Route path="/neet/online-coaching-class-12" element={<Class12Program />} />
+          <Route path="/" element={<Landing />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
+}
+
+function Landing() {
+  return <div>
+    Home page
+  </div>
+}
+
+function Class11Program() {
+  return <div>
+    Class 11th content is here which you are looking for...
+  </div>
+}
+
+function Class12Program() {
+  return <div>
+    Class 12th content is here which you are looking for...
+  </div>
 }
 
 export default App
