13947903728e1b53b66f7843a56dc4bd79260c30
Dhiraj-Makwana <dhirajmakwana33912@gmail.com>
2025-12-22 23:14:14 +0530

    Counter with Inc/Dec functionalities to understand how extra re-renders are happen in Cntext API

--- Week-10/React-9/src/App.jsx
+++ Week-10/React-9/src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { createContext, useContext, useState } from 'react'
+
+const CountContext = createContext()
+
+function CountProvider({ children }) {
+  const [count, setCount] = useState(0)
+  
+  return <CountContext.Provider value={{
+    count: count,
+    setCount: setCount
+  }}>
+    {children}
+  </CountContext.Provider>
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <CountProvider>
+      <Parent />
+      </CountProvider>
+    </div>
   )
+}
+
+function Parent() {
+  return <div>
+    <Increase />
+    <Decrease />
+    <Value />
+  </div>
+}
+
+function Increase() {
+  const { setCount } = useContext(CountContext)
+  return <button onClick={() => setCount(count => count + 1)}>Increase</button>
+}
+
+function Decrease() {
+  const { setCount } = useContext(CountContext)
+  return <button onClick={() => setCount(count => count - 1)}>Decrease</button>
+}
+
+function Value() {
+  const { count } = useContext(CountContext)
+  return <p>Count: {count}</p>
 }
 
 export default App
