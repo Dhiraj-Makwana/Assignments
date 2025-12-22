8e9e693f17b0417f73cea507432f0d7c9e8ea8bc
Dhiraj-Makwana <dhirajmakwana33912@gmail.com>
2025-12-22 11:10:56 +0530

    Write Light-Bulb feature logic to understand Rolling up state to LCA(Least Common Ancestor)

--- Week-10/React-8/src/App.jsx
+++ Week-10/React-8/src/App.jsx
@@ -1,13 +1,38 @@
-
+import { useState } from 'react'
 
 function App() {
-  
 
   return (
     <div>
-      Hi there 2
+      <LightBulb />
     </div>
   )
+}
+
+function LightBulb() {
+  const [bulbOn, setBulbOn] = useState(false);
+
+  return <div>
+    <BulbState bulbOn={bulbOn} />
+    <ToggleBulbState bulbOn={bulbOn} setBulbOn={setBulbOn} />
+  </div>
+}
+
+function BulbState({bulbOn}) {
+  return <div>
+    {bulbOn ? "Bulb on" : "Bulb off"}
+  </div>
+}
+
+function ToggleBulbState({bulbOn, setBulbOn}) {
+  function Toggle() {
+    //setBulbOn(currentState => !currentState)
+    setBulbOn(!bulbOn)
+  }
+
+  return <div>
+    <button onClick={Toggle}>Toggle Bulb</button>
+  </div>
 }
 
 export default App
