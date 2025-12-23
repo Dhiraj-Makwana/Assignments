efee55a194d290a6d82e43971befa90aeeac96a7
Dhiraj-Makwana <dhirajmakwana33912@gmail.com>
2025-12-23 22:17:01 +0530

    function linearSearch

--- AsyncJS/linearSearch.java
+++ AsyncJS/linearSearch.java
@@ -1,12 +1,22 @@
 public class linearSearch{
-    public static void main(String args[]) {
-        int arr[] = {5,3,6,7,3,1,0};
-        int target = 10;
-
+    public static int search(int arr[], int target) {
         for(int i=0; i < arr.length; i++) {
             if(arr[i] == target) {
-                System.out.println(target+ " is at index "+ i);
+                return i;
             }
+        }
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {5,3,6,7,3,1,0};
+        int target = 0;
+
+        if(search(arr, target) != -1) {
+            System.out.println(target+ " is at index "+ search(arr, target));
+        }
+        else {
+            System.out.println(target+ " is not found");
         }
     }
 }
