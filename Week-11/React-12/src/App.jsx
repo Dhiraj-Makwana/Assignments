47decade38b5a9853095dd3ce2489861765476a4
Dhiraj-Makwana <dhirajmakwana33912@gmail.com>
2025-12-26 22:41:36 +0530

    Add Increase,Decrease,count(Even/Odd) to see extra re-renders

--- Week-11/React-12/src/App.jsx
+++ Week-11/React-12/src/App.jsx
@@ -1,12 +1,43 @@
 import './App.css'
+import { useState } from "react"
+import { RecoilRoot } from "recoil"
 
 function App() {
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      Hi there
+      <Counter count={count} />
+      <Increase setCount={setCount} />
+      <Decrease setCount={setCount}/>
+      <IsEven count={count} />
     </>
   )
+}
+
+function IsEven({count}) {
+  const bin = count%2 == 0 ? "Even" : "Odd"
+  return <h1>{bin}</h1>
+}
+
+function Counter({count}) {
+  return <div>{count}</div>
+}
+
+function Increase({setCount}) {
+  function Inc() {
+    setCount(c => c + 2)
+  }
+
+  return <button onClick={Inc}>Increase</button>
+}
+
+function Decrease({setCount}) {
+  function Dec() {
+    setCount(c => c - 1)
+  }
+
+  return <button onClick={Dec}>Decrease</button>
 }
 
 export default App
