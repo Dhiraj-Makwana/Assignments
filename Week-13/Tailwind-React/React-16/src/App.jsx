7eaa824573089cba49e9238dfd8397a09c2a9dff
Dhiraj-Makwana <dhirajmakwana33912@gmail.com>
2025-12-29 10:59:43 +0530

    implement grid in tailwind

--- Week-13/Tailwind-React/React-16/src/App.jsx
+++ Week-13/Tailwind-React/React-16/src/App.jsx
@@ -4,7 +4,11 @@ function App() {
 
   return (
     <>
-      <div className='bg-blue-300'>Starting to implement Tailwind CSS</div>
+      <div className='grid grid-cols-12'>
+        <div className='col-span-4 bg-red-300'>Child - 1</div>
+        <div className='bg-blue-300 col-span-6'>Child - 2</div>
+        <div className='bg-green-300 col-span-2'>Child - 3</div>
+      </div>
     </>
   )
 }
