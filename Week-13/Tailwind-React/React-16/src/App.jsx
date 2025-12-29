2642e6f0e776c8e80565cd0e16422b19b69002de
Dhiraj-Makwana <dhirajmakwana33912@gmail.com>
2025-12-29 11:15:40 +0530

    complete first assignment

--- Week-13/Tailwind-React/React-16/src/App.jsx
+++ Week-13/Tailwind-React/React-16/src/App.jsx
@@ -4,10 +4,10 @@ function App() {
 
   return (
     <>
-      <div className='grid grid-cols-12'>
-        <div className='col-span-4 bg-red-300'>Child - 1</div>
-        <div className='bg-blue-300 col-span-6'>Child - 2</div>
-        <div className='bg-green-300 col-span-2'>Child - 3</div>
+      <div className='sm:grid grid-cols-12'>
+        <div className='bg-red-300 sm:col-span-6'>Child - 1</div>
+        <div className='bg-blue-300 sm:col-span-4'>Child - 2</div>
+        <div className='bg-green-300 sm:col-span-2'>Child - 3</div>
       </div>
     </>
   )
