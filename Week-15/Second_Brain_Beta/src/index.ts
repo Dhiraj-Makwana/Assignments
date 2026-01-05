36d605e6c0912c3105454defbbdc70565522778a
Dhiraj-Makwana <dhirajmakwana33912@gmail.com>
2026-01-05 10:01:32 +0530

    Add signup api endpoint with zod validation & hashed password

---
+++ Week-15/Second_Brain_Beta/src/index.ts
@@ -0,0 +1,131 @@
+import express from "express";
+import { UserModel, ContentModel } from "./db.js";
+import { userMiddleware } from "./Middleware.js";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+import z from "zod";
+import bcrypt from "bcrypt";
+
+const JWT_SECRET = "World$Be$tProgrammer";
+const app = express();
+app.use(express.json());
+
+/*----------SignUp_&_SignIn----------*/
+const SignUpSchema = z.object({
+  username: z
+    .string()
+    .min(3)
+    .max(10)
+    .regex(/^[A-Za-z]+$/, "Username must contain only letters"),
+
+  password: z
+    .string()
+    .min(8)
+    .max(20)
+    .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+    .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+    .regex(/[0-9]/, "Password must contain at least one number")
+    .regex(
+      /[^A-Za-z0-9]/,
+      "Password must contain at least one special character"
+    ),
+});
+
+app.post("/api/v1/signup", async (req, res) => {
+  //try catch, zod validation, hash password
+  const result = SignUpSchema.safeParse(req.body);
+
+  if (!result.success) {
+    return res.status(400).json({
+      errors: result.error,
+    });
+  }
+
+  const { username, password } = result.data;
+
+  //hash password using bcrypt
+  const hashedPassword = await bcrypt.hash(password, 8);
+
+  try {
+    await UserModel.create({
+      username: username,
+      password: hashedPassword,
+    });
+
+    res.json({
+      message: "User is Signed up",
+    });
+  } catch (error) {
+    res.json({
+      error: "User already exist:( Please Sign in",
+    });
+  }
+});
+
+app.post("/api/v1/signin", async (req, res) => {
+//   const result = SignUpSchema.safeParse(req.body);
+
+//   if (!result.success) {
+//     return res.status(400).json({
+//       errors: result.error,
+//     });
+//   }
+
+//   const { username, password } = result.data;
+
+//   const existingUser = await UserModel.findOne({ username });
+
+//   if (!existingUser) {
+//     return res.status(403).json({
+//       message: "Incorrect Credentials",
+//     });
+//   }
+//     const isPasswordCorrect = await bcrypt.compare(
+//       password,
+//       existingUser.password
+//     );
+
+//     if (!isPasswordCorrect) {
+//       res.status(403).json({
+//         message: "Incorrect Credentials",
+//       });
+//     }
+
+//     const token = jwt.sign( { id: existingUser._id }, JWT_SECRET );
+
+//     return res.json({ token });
+});
+
+app.post("/api/v1/content", userMiddleware, async (req, res) => {
+//   const link = req.body.link;
+//   const title = req.body.title;
+//   const type = req.body.type;
+
+//   await ContentModel.create({
+//     link,
+//     title,
+//     type,
+//     userId: new mongoose.Types.ObjectId(req.userId),
+//     tags: [],
+//   });
+//   return res.json({
+//     message: "Content is added",
+//   });
+// });
+
+// app.get("/api/v1/content", userMiddleware, async (req, res) => {
+//   const content = await ContentModel.find({
+//     userId: new mongoose.Types.ObjectId(req.userId),
+//   }).populate("userId", "username");
+//   res.json({
+//     content,
+//   });
+});
+
+app.delete("/api/v1/content", userMiddleware, (req, res) => {});
+
+app.post("/api/v1/brain/share", userMiddleware, (req, res) => {});
+
+app.get("/api/v1/brain/:shareLink", (req, res) => {});
+
+app.listen(3000);
