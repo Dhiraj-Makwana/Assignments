523ec0400fc853db652764d8e5d7f8ed0bb47bf6
Dhiraj-Makwana <dhirajmakwana33912@gmail.com>
2026-01-12 01:17:55 +0530

    Create a ping-pong (React + WebSocket) app

--- Week-17/WebSocket-Client/src/App.tsx
+++ Week-17/WebSocket-Client/src/App.tsx
@@ -1,10 +1,36 @@
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [socket, setSocket] = useState<WebSocket | null>(null)
+  const inputRef = useRef<HTMLInputElement>(null)
+  
+  function sendMessage() {
+    if(!socket) {
+      return
+    }
+    const message = inputRef.current?.value 
+    if(message) {
+      socket.send(message)
+    }
+  }
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8080")
+
+    ws.onopen = () => {
+      setSocket(ws)
+    }
+
+    ws.onmessage = (ev) => {
+      alert(ev.data)
+    }
+  }, [])
 
   return (
     <div>
-      Starting ping-pong webSocket app
+      <input ref={inputRef} type="text" placeholder='write ping' />
+      <button onClick={sendMessage} type="button">Send</button>
     </div>
   )
 }
