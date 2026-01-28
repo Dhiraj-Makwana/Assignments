0a3e43261fed4a91fd31e54ef9d556b67e26e7b3
Dhiraj-Makwana <dhirajmakwana33912@gmail.com>
2026-01-28 11:54:48 +0530

    Update Week-18

---
+++ Week-18/src/index.ts
@@ -0,0 +1,3 @@
+import { Client } from 'pg'
+
+const pgClient = new Client("postgresql://neondb_owner:npg_0NOvFd3lykWe@ep-hidden-mouse-ah5hkd6w-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
